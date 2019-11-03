19a7a83078643d3501cc4c5f69069b3adbb29d24
asd4486 <vivienwang54@gmail.com>
2019-11-03 23:33:51 +0100

    cleaning project

---
+++ Assets/DOKIVnMaker/Scripts/OLDItems/SoundManager.cs
@@ -0,0 +1,51 @@
+﻿using DokiVnMaker.MyEditor.Items;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace DokiVnMaker.Game
+{
+    public class SoundManager : MonoBehaviour
+    {
+        [SerializeField] AudioSource backgroundMusicSources;
+
+        [SerializeField] List<AudioSource> soundSources;
+        [SerializeField, Range(0, 1)] float globalVolume = 1;
+
+        public void PlaySound(Sound action)
+        {
+            if(soundSources.Count -1 < action.trackIndex)
+            {
+                for(int i=0; i < action.trackIndex + 1 - soundSources.Count; i++)
+                {
+                    soundSources.Add(null);
+                }
+
+                var o = new GameObject("sound");
+                o.transform.SetParent(transform, false);
+                soundSources[action.trackIndex] = o.AddComponent<AudioSource>();
+                SetupAudio(soundSources[action.trackIndex], action);
+            }
+            
+        }
+
+        public void PlayBackgroundMusic(Music action)
+        {
+            if(backgroundMusicSources == null)
+            {
+                backgroundMusicSources = new GameObject("background_music").AddComponent<AudioSource>();
+            }
+            SetupAudio(backgroundMusicSources, action);
+        }
+
+        void SetupAudio(AudioSource audioSource, AudioBase action)
+        {
+            var audio = AssetDatabase.LoadAssetAtPath(action.path, typeof(AudioClip)) as AudioClip;
+            if (audio != null) audioSource.clip = audio;
+
+            audioSource.volume = action.volume;
+            audioSource.loop = action.loop;
+        }
+    }
+}
