b4714c9f5e41a47506e035edc2e0d1de5db416ad
asd4486 <vivienwang54@gmail.com>
2019-11-08 01:08:54 +0100

    fix gitignore bug

---
+++ Assets/DOKIVnMaker/Scripts/Game/DokiGameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DokiVnMaker.Game
+{
+    public class DokiGameManager : MonoBehaviour, ISerializationCallbackReceiver
+    {
+        [SerializeField] TextStyleObject dialogueTextStyle;
+        public static TextStyleObject DialogueTextStyle { get; private set; }
+
+        [SerializeField] CGManagerObject cgManager;
+        public static CGManagerObject CgManager { get; private set; }
+
+        public void ChangeDialogueTextStyle(TextStyleObject style)
+        {
+            DialogueTextStyle = dialogueTextStyle;
+        }
+
+        public void OnAfterDeserialize()
+        {
+            DialogueTextStyle = dialogueTextStyle;
+            CgManager = cgManager;
+        }
+
+        public void OnBeforeSerialize()
+        {
+        }
+    }
+}
